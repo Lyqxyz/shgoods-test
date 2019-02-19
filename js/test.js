44fea0278f70fc7311a1335f82a67f0e5389bae3
Your Name <you@example.com>
2019-02-19 20:52:51 +0800

    first commit

---
+++ js/test.js
@@ -0,0 +1,15 @@
+let user={
+        userNum:'ss',
+        userName:'ss',
+        userPhone:'s',
+        userEmail:'s',
+        userGender:'s',
+        userPwd:'s',
+        userCollege:'ss',
+}
+
+
+for( (key ,value) in user){
+
+    console.log(key+value);
+}
